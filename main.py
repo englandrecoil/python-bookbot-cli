6c29e9266f86f0e5f57d9c5d868d74e809ce7876
englandrecoil <nik.tereshchenko.982@gmail.com>
2025-10-23 03:46:02 +0300

    add function to count the words in a book

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ def get_book_text(path: str) -> str:
     if path == "" or path == None:
         raise Exception("path parameter must be provided!")
     
-    with open(path) as f:
-        data = f.read()
-    return data
+    try:
+        with open(path) as f:
+            return f.read()
+    except Exception as e:
+        raise Exception(e)
+
+def count_book_words(text: str) -> int:
+    data = text.split()
+    return len(data)
 
 def main():
     path = "books/frankenstein.txt"
     book_text = get_book_text(path)
-    print(book_text)
+    words_count = count_book_words(book_text)
+    print(f"Found {words_count} total words")
 
 main()
