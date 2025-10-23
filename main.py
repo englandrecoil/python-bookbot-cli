3826e9542ed9335098b81199a156315e33791e67
englandrecoil <nik.tereshchenko.982@gmail.com>
2025-10-23 05:19:56 +0300

    add book path support via sys.argv

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from stats import *
+from stats import (
+    count_num_words, 
+    get_chars_dict, 
+    sort_dict,
+)
+import sys
 
 def main():
-    path = "books/frankenstein.txt"
+    args = sys.argv
+    if len(args) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path=args[1]
     try:
         book_text = get_book_text(path)
         words_count = count_num_words(book_text)
